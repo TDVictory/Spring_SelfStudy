05e7d917bfc73df5ad06952f744739c9f530f753
邓元俊 <598779014@qq.com>
2019-07-31 11:58:47 +0800

    更新SpringBoot学习Demo

---
+++ SpringBoot_Project/SpringBoot_Demo/src/main/java/com/TDVictory/HelloWorldMainApplication.java
@@ -0,0 +1,16 @@
+package com.TDVictory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class HelloWorldMainApplication {
+
+    public static void main(String[] args) {
+
+        //启动Spring
+        SpringApplication.run(HelloWorldMainApplication.class,args);
+    }
+
+}
